b276ad873e9f582af4f1686bb276eef812b7ef19
Mitchell <mitchell.cifuentes@gmail.com>
2018-09-20 16:25:06 -0300

    lesson02

---
+++ app/src/main/java/com/cifuentes/mitchell/swoosh/LeagueActivity.kt
@@ -0,0 +1,12 @@
+package com.cifuentes.mitchell.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
