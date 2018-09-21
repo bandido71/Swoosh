8c4041c71dddffbd4d0698b785fc090ac87af51e
Mitchell <mitchell.cifuentes@gmail.com>
2018-09-20 22:42:49 -0300

    lesson06

---
+++ app/src/main/java/com/cifuentes/mitchell/swoosh/SkillActivity.kt
@@ -0,0 +1,12 @@
+package com.cifuentes.mitchell.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
